399191d03b41b1463712cd5f5c4a217b97790e00
KevinB <k1@k9b9.com>
2019-04-04 11:02:35 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+
+
+Good [StackOverflow answer](https://stackoverflow.com/questions/21503883/spring-boot-application-as-a-service/22121547#22121547) for setting up a systemd unit file.
---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# no need to sudo su, EC2 user-data scripts are run as root
+# update the image
+yum update -y
+# install Maria (MySQL fork)
+yum install -y mariadb-server
+service mariadb start
+# install Java (Amazon Corretto 8)
+# enable the corretto repo
+amazon-linux-extras enable corretto8
+# install the JDK, not the JRE
+yum install -y java-1.8.0-amazon-corretto-devel
+# setup the docker compose service definition
+mkdir /srv/app
+curl -o /srv/app/app.jar https://s3.amazonaws.com/acg-cors.kevinbaynes.com/public-jars/hello-0.0.1-SNAPSHOT.jar
+# make the ec2 user the owner of the app service folder
+chown -R ec2-user: /srv/app
+# setup systemd unit file to run compose app as a service
+curl -o /etc/systemd/system/java-app.service https://raw.githubusercontent.com/kbaynes/ec2-l2-java-mariadb/master/app.service
+systemctl enable java-app
+
+systemctl start java-app
