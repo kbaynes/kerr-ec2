750e28205344e30a60ee4d2b66e1086b05cf425e
KevinB <k1@k9b9.com>
2019-04-04 18:24:14 -0400

    setting up iptables as systemd unit file

--- setup.sh
+++ setup.sh
@@ -11,22 +11,18 @@ service mariadb start
 amazon-linux-extras enable corretto8
 # install the JDK, not the JRE
 yum install -y java-1.8.0-amazon-corretto-devel
-# setup the docker compose service definition
+# setup the app location and download the jar - runs Tomcat on 8080
 mkdir /srv/app
 curl -o /srv/app/app.jar https://s3.amazonaws.com/acg-cors.kevinbaynes.com/public-jars/hello-0.0.4.jar
 # make the ec2 user the owner of the app service folder
 chown -R ec2-user: /srv/app
 # map calls to port 80 on this machine to port 8080, where the java-app is listening
 # otherwise, if we try to run java-app on port 80, it must be run as root (security problem)
-# iptables are not persistent - see setup-iptables.sh
-# download iptables mapping script
-curl -o /srv/app/setup-iptables.sh https://raw.githubusercontent.com/kbaynes/ec2-l2-java-mariadb/master/setup-iptables.sh
-# run the mapping
-/bin/bash /srv/app/setup-iptables.sh
-# set a cron to re-run the mapping on reboot (works on Amazon Linux 2)
-(crontab -l 2>/dev/null; echo "@reboot /bin/bash /srv/app/setup-iptables.sh") | crontab -
+# setup systemd unit file to run iptable mapping as a service (iptables are not persistent)
+curl -o /etc/systemd/system/java-app-onstartup.service https://raw.githubusercontent.com/kbaynes/ec2-l2-java-mariadb/master/app-onstartup.service
+systemctl enable java-app-onstartup
+systemctl start java-app-onstartup
 # setup systemd unit file to run compose app as a service
 curl -o /etc/systemd/system/java-app.service https://raw.githubusercontent.com/kbaynes/ec2-l2-java-mariadb/master/app.service
 systemctl enable java-app
-
 systemctl start java-app
